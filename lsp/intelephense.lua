3d0158aaca713154a0863c5dcecdcb4ffbbb32f9
Diego Vazquez <diurivj@gmail.com>
2025-07-10 11:17:47 -0600

    Fixing intelephense config

--- lsp/intelephense.lua
+++ lsp/intelephense.lua
@@ -1,18 +1,38 @@
 local blink = require("blink.cmp")
 
-local get_intelephense_license = function()
-  local f = assert(io.open(os.getenv("HOME") .. "/intelephense/licence.txt", "rb"))
-  local content = f:read("*a")
-  f:close()
-  return string.gsub(content, "%s+", "")
-end
-
 return {
   cmd = { "intelephense", "--stdio" },
   filetypes = { "php", "blade" },
   root_markers = { "composer.json", ".git" },
-  init_options = {
-    licenceKey = get_intelephense_license(),
+  settings = {
+    intelephense = {
+      files = {
+        maxSize = 1000000,
+        exclude = {
+          "**/.git/**",
+          "**/.svn/**",
+          "**/.hg/**",
+          "**/CVS/**",
+          "**/.DS_Store/**",
+          "**/node_modules/**",
+          "**/bower_components/**",
+          "**/htdocs_customshops/**",
+          "**/htdocs_gearman/**",
+          "**/htdocs/assets/dist/**",
+          "**/tmp/**",
+          "**vendor/*/{!(phpunit)/**}",
+          "translations/**",
+          "**/.phan/**",
+          "**/cron.d/**",
+          "**/generated/**",
+          "**/Generated/**",
+          "**/modules/css/**",
+          "**/__modules__*__src__/**",
+          "vendor/etsy/module-*/**",
+        },
+      },
+      maxMemory = 10192,
+    },
   },
   capabilities = vim.tbl_deep_extend(
     "force",
