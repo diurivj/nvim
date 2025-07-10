8490d8fc2de9614d0e081a865a2edfc4418e6aab
Diego Vazquez <diurivj@gmail.com>
2025-07-10 11:23:56 -0600

    make sure mason installs deps

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,30 @@
 return {
   "mason-org/mason.nvim",
-  opts = {},
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    require("mason").setup()
+    
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "lua_ls",
+        "ts_ls",
+        "intelephense",
+        "tailwindcss",
+      },
+      automatic_installation = true,
+    })
+    
+    require("mason-tool-installer").setup({
+      ensure_installed = {
+        "stylua",
+        "prettier",
+        "prettierd",
+      },
+      auto_update = false,
+      run_on_start = true,
+    })
+  end,
 }
