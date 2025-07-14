8c39ce61225038312894736292a894777e0cc55b
Diego Vazquez <diurivj@gmail.com>
2025-07-14 11:33:39 -0600

    fixing lua-line

--- lua/plugins/lua-line.lua
+++ lua/plugins/lua-line.lua
@@ -1,5 +1,26 @@
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {},
+  opts = {
+    options = {
+      theme = "auto",
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
+    },
+    sections = {
+      lualine_c = {
+        {
+          "filename",
+          path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+          shorting_target = 40,
+          symbols = {
+            modified = " ●",
+            readonly = " ",
+            unnamed = "[No Name]",
+            newfile = " NEW",
+          },
+        },
+      },
+    },
+  },
 }
